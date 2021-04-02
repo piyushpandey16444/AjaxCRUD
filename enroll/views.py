b6ccdf7ab9be4e2998f3a6c5b961ac9272d2b2ae
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-02 23:25:55 +0530

    index updated

--- enroll/views.py
+++ enroll/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .forms import User
 from .forms import UserForm
 
 
 def home_view(request):
     form = UserForm()
-    context = {'form': form}
+    user_objs = get_list_or_404(User)
+    context = {'form': form, 'users': user_objs}
     return render(request, 'enroll/home.html', context=context)

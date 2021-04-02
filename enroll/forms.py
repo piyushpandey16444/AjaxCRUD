be9e709ab9c55e6b25ff3514194b8c0b89247e89
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-02 23:15:42 +0530

    migrations done

--- enroll/forms.py
+++ enroll/forms.py
@@ -6,3 +6,8 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["name", "email", "password"]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'form__name'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'id': 'form__email'}),
+            'password': forms.TextInput(attrs={'class': 'form-control', 'id': 'form__password'}),
+        }

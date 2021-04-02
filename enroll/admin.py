5089ed575b6ee543477fe072d5705b5d9e78f139
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-02 22:55:45 +0530

    forms file added

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'password']
+    list_display_links = ['name', 'email', 'password']
 
 # Register your models here.
